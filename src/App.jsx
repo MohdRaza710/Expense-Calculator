f45e6305f0502a34c8b01813f64fe5474f8bce61
MohdRaza710 <mrmraza.710@gmail.com>
2025-01-16 16:33:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { ExpenseProvider } from './context'
+import { ExpenseProvider } from './context/ExpenseContext'
 import  ExpenseForm  from './Components/ExpenseForm'
 import  ExpenseItem  from './Components/ExpenseItem'
 function App() {
